f64f27c66bdd5f5cd476233724be88ac57e0575d
dedigurova <alenadedigurova@yandex.ru>
2013-11-23 13:43:13 +0700

    communication between clients

--- server.c
+++ server.c
@@ -23,6 +23,7 @@ typedef struct clients_description {
 	pthread_t thread_ID;
 	int sockfd;
 	int partner_ID;
+	int busy;
 } clients_t;
 
 clients_t clients_struct[MAX];
@@ -34,6 +35,7 @@ void initial_sessions()
 		clients_struct[i].index = i;
 		clients_struct[i].sockfd = -1;
 		clients_struct[i].partner_ID = -1;
+		clients_struct[i].busy = 1;
 	}
 }
 
@@ -48,7 +50,7 @@ int search_available()
 	return -1;
 }
 /*
-void send_to_all(clients_t *sender, char *buf)
+void send_to_all(char *buf)
 {
 	int i;
 	for (i = 0; i < MAX; i++){
@@ -90,7 +92,11 @@ void clients_choise_handler(clients_t *me, int choise)
 {
 	clients_struct[choise].partner_ID = me->sockfd;
 	me->partner_ID = clients_struct[choise].sockfd;
-	printf("мой sockfd у моего партнера_%d sock моего партнера_%d\n\n", clients_struct[choise].partner_ID, me->partner_ID);
+	printf("мой sockfd у моего партнера_%d sock моего партнера_%d\n\n", 
+			clients_struct[choise].partner_ID, me->partner_ID);
+	me->busy = 0;
+	clients_struct[choise].busy = 0;
+	
 }
 	
 			
@@ -100,11 +106,11 @@ void *threads_handler(void * session_index)
 	char buf[BUF_SIZE];
 	char convert[2];
 	int client_s;
-	int choosing = 1;
 	client_s = *((int *)session_index);
-	printf("client_s %d__sockfd__%d___index is %d\n", client_s, clients_struct[client_s].sockfd, clients_struct[client_s].index);
+	printf("client_s %d__sockfd__%d___index is %d\n", client_s, clients_struct[client_s].sockfd,
+		   clients_struct[client_s].index);
 	bzero((char *)buf, sizeof(buf[0]) * BUF_SIZE);
-	while(choosing){
+	while(clients_struct[client_s].busy){
 		recvd = recv(clients_struct[client_s].sockfd, buf, sizeof(buf), 0);
 		if (recvd <= 0){
 		fprintf(stderr, "choosing error!!\n");
@@ -127,24 +133,26 @@ void *threads_handler(void * session_index)
 			}
 			bzero((char *)buf, sizeof(buf[0]) * BUF_SIZE);
 		}
-	}/*
-	while(1){
-		recvd = recv(clients_struct[client_s].partner_ID, buf, sizeof(buf), 0);
-		printf("%d", recvd);
-		
+	}
+	sprintf(buf, "hey i am your partner: %d\n", clients_struct[client_s].sockfd);
+	send(clients_struct[client_s].partner_ID, buf, 26, 0);
+	while (1)
+	{
+		recvd = recv(clients_struct[client_s].sockfd, buf, sizeof(buf), 0);
 		if (recvd <= 0){
 			fprintf(stderr, "error recvd < 0!!!\n");
 			break;
-		}	
+		}
+		
 		if (recvd > 0){
 			printf("\n");
 			printf("got it__%s__from__%d\n", buf, clients_struct[client_s].partner_ID);
-			pthread_mutex_lock(&mutex);
-			send(clients_struct[client_s].partner_ID, "hello", 5, 0);
-			pthread_mutex_unlock(&mutex);
+			//pthread_mutex_lock(&mutex);
+			send(clients_struct[client_s].partner_ID, buf, sizeof(buf), 0);
+			//pthread_mutex_unlock(&mutex);
 		}
 		bzero((char *)buf, sizeof(buf[0]) * BUF_SIZE);
-	}*/
+	}
 		close(clients_struct[client_s].sockfd);
 		clients_struct[client_s].sockfd = -1;
 		clients_struct[client_s].partner_ID = -1;
@@ -157,7 +165,6 @@ int main(int argc, char *argv[])
 	int sock, connection, port, aid;
 	int cur_index;
 	struct sockaddr_in server, client;
-	//pthread_t threads;
 	socklen_t len = sizeof(struct sockaddr_in);
 	if (argc != 3){
 		printf("мало аргументов командной строки!\n"); //первый аргумент порт, второй max_client
@@ -204,7 +211,6 @@ int main(int argc, char *argv[])
 			printf("accept() error!\n");
 			break;
 		}
-		//sleep(3);
 	}
 	remove("save");	
 	close(connection);
